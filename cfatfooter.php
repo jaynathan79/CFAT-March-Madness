547b64050c5be533fd0be2f1bbf354b1a01034b9
Jeremie Weldin <jweldin@jweldin.com>
2011-02-28 05:14:48 -0500

    created header/footer includes and a register.php

---
+++ cfatfooter.php
@@ -0,0 +1,16 @@
+<!-- begin footer -->
+	</div>
+</section>
+
+<footer>
+	<nav class="round">
+		<ul>
+			<li><a href="/">About</a></li>
+			<li><a href="/">Contact</a></li>
+		</ul>
+	</nav>
+</footer>
+
+</div>
+</body>
+</html>
---
+++ cfatheader.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>[TITLE]</title>
+<link href='css/blueprint/screen.css' media='screen' rel='stylesheet' type='text/css' />
+<link href='css/custom.css' media='screen' rel='stylesheet' type='text/css' />
+</head>
+<body>
+	<div class="container">
+		<header>
+			<img src="images/cfattemplogo.png" border="0">
+			<nav class="round login">
+				<ul>
+					<li><a href="/">Login</a></li>
+				</ul>
+			</nav>
+			<nav class="round">
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/">Blog</a></li>
+				</ul>
+			</nav>
+			
+		</header>
+		
+		<section class="round content">
+			<div id="padded">
+		<!-- end header -->
